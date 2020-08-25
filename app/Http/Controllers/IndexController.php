d5bc1b69d2f487cbca6f6f641a7c915cf88d99fa
SoninVI <v.sonin1986@gmail.com>
2020-08-25 08:05:56 +0300

    form for create advet.

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,24 +6,37 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    public function getIndex() {
-            if(view()->exists('index')) {
-                return view('index', ['title' => 'GrainBoard | Главная']);
-            }
-            abort(404);
-    }
-
-    public function createAdv(Request $request) {
-        if($request->isMethod('post')) {
-            $request->flash();
-
-            // return redirect()->route('createAdv'); 
-            // echo "ok";
-        }
-
-        if(view()->exists('form')) {
-            return view('form', ['title' => 'GrainBoard | Создать']);
+    public function getIndex()
+    {
+        if (view()->exists('index')) {
+            return view('index', ['title' => 'GrainBoard | Главная']);
         }
         abort(404);
-}
+    }
+
+    public function createAdv(Request $request)
+    {
+        if ($request->isMethod('post')) {
+            
+        }
+
+        if (view()->exists('form')) {
+            $crops = \DB::table('crops')->get()->where('available', 'yes');
+            $crops_classiness = \DB::table('crops_classiness')->get()->where('available', 'yes');
+            $incoterms = \DB::table('incoterms')->get()->where('available', 'yes');
+            $locations = \DB::table('location_regions_districts')
+            ->join('location_regions', 'location_regions_districts.region_id', '=', 'location_regions.id')
+            ->select('location_regions_districts.*', 'location_regions.name as regions_name')
+            ->get();
+            $seller_types = \DB::table('seller_types')->get()->where('available', 'yes');
+            $pickup = \DB::table('pickup')->get()->where('available', 'yes');
+            $payment_methods = \DB::table('payment_methods')->get()->where('available', 'yes');
+            $payment_forms = \DB::table('payment_forms')->get();
+            return view('form', ['title' => 'GrainBoard | Создать'], ['crops' => $crops, 'crops_classiness' => $crops_classiness, 
+                                  'incoterms' => $incoterms, 'locations' => $locations, 
+                                  'seller_types' => $seller_types, 'pickup' => $pickup, 'payment_methods' => $payment_methods, 
+                                  'payment_forms' => $payment_forms]);
+        }
+        abort(404);
+    }
 }
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,162 +1,151 @@
 @extends('layouts.layout')
 
 @section('sidebar')
-    
+
 @endsection
 @section('main_content')
-            
+
 @endsection
 
 @section('form')
 
-<!-- <pre>
-{{ print_r(Session::all()) }}
-</pre> -->
+<div class="row">
+  <form novalidate="" action="{{ route('createAdv') }}" method="POST">
+    @csrf
+    <h4 class="mb-3">Опишите товарную позицию:</h4>
+    <div class="row">
+      <div class="col-md-12 mb-3">
+        <label for="crop">Наименование зерновой культуры:</label>
+        <select class="custom-select d-block w-100" id="crop" name='crop' required="">
+          <option value="">Choose...</option>
+          @forelse ($crops as $crop)
+          <option value="{{$crop->id}}">{{$crop->name}}</option>
+          @empty
+          <p>Отсутствуют</p>
+          @endforelse
+        </select>
+      </div>
+      <div class="col-md-6 mb-3">
+        <label for="crops_class">Классификация технологического качества (класс):</label>
+        <select class="custom-select d-block w-100" id="crops_class" name='crops_class' required="">
+          <option value="">Choose...</option>
 
-<form class="needs-validation" novalidate="" method="post" action="{{ route('createAdv') }}">
-@csrf
-        <div class="row">
-          <div class="col-md-6 mb-3">
-            <label for="firstName">First name</label>
-            <input type="text" class="form-control" id="firstName" placeholder="" value="{{ old('first_name') }}" required="" name="first_name">
-            <div class="invalid-feedback">
-              Valid first name is required.
-            </div>
-          </div>
-          <div class="col-md-6 mb-3">
-            <label for="lastName">Last name</label>
-            <input type="text" class="form-control" id="lastName" placeholder="" value="{{ old('last_name') }}" required="" name="last_name">
-            <div class="invalid-feedback">
-              Valid last name is required.
-            </div>
-          </div>
-        </div>
+          @forelse ($crops_classiness as $crops_class)
+          <option value="{{$crops_class->id}}">{{$crops_class->name}}</option>
+          @empty
+          <p>Отсутствуют</p>
+          @endforelse
 
-        <div class="mb-3">
-          <label for="username">Username</label>
-          <div class="input-group">
-            <div class="input-group-prepend">
-              <span class="input-group-text">@</span>
-            </div>
-            <input type="text" class="form-control" id="username" placeholder="Username" required="" value="{{ old('user_name') }}" name="user_name">
-            <div class="invalid-feedback" style="width: 100%;">
-              Your username is required.
-            </div>
-          </div>
-        </div>
+        </select>
+      </div>
+      <div class="col-md-6 mb-3">
+        <label for="incoterms">ИНКОТЕРМС (международные правила и условия поставки):</label>
+        <select class="custom-select d-block w-100" id="incoterms" name='incoterms' required="">
+          <option value="">Choose...</option>
 
-        <div class="mb-3">
-          <label for="email">Email <span class="text-muted">(Optional)</span></label>
-          <input type="email" class="form-control" id="email" placeholder="you@example.com" name="email" value="{{ old('email') }}">
-          <div class="invalid-feedback">
-            Please enter a valid email address for shipping updates.
-          </div>
-        </div>
+          @forelse ($incoterms as $incoterm)
+          <option value="{{$incoterm->id}}">{{$incoterm->abbr}}</option>
+          @empty
+          <p>Отсутствуют</p>
+          @endforelse
 
-        <div class="mb-3">
-          <label for="address">Address</label>
-          <input type="text" class="form-control" id="address" placeholder="1234 Main St" required="" name="address" value="{{ old('address') }}">
-          <div class="invalid-feedback">
-            Please enter your shipping address.
-          </div>
-        </div>
+        </select>
+      </div>
+      
 
-        <div class="mb-3">
-          <label for="address2">Address 2 <span class="text-muted">(Optional)</span></label>
-          <input type="text" class="form-control" id="address2" placeholder="Apartment or suite" value="{{ old('address2') }}" name="address2">
-        </div>
+      <div class="col-md-6 mb-3">
+        <label for="districts">Район:</label>
+        <select class="custom-select d-block w-100" id="districts" name='districts' required="">
+          <option value="">Choose...</option>
 
-        <div class="row">
-          <div class="col-md-5 mb-3">
-            <label for="country">Country</label>
-            <select class="custom-select d-block w-100" id="country" required="" name="country">
-              <option value="">Choose...</option>
-              <option value="United States">United States</option>
-            </select>
-            <div class="invalid-feedback">
-              Please select a valid country.
-            </div>
-          </div>
-          <div class="col-md-4 mb-3">
-            <label for="state">State</label>
-            <select class="custom-select d-block w-100" id="state" required="" name="state">
-              <option value="">Choose...</option>
-              <option value="California">California</option>
-            </select>
-            <div class="invalid-feedback">
-              Please provide a valid state.
-            </div>
-          </div>
-          <div class="col-md-3 mb-3">
-            <label for="zip">Zip</label>
-            <input type="text" class="form-control" id="zip" placeholder="" required="" name="zip">
-            <div class="invalid-feedback">
-              Zip code required.
-            </div>
-          </div>
-        </div>
-        <hr class="mb-4">
-        <div class="custom-control custom-checkbox">
-          <input type="checkbox" class="custom-control-input" id="same-address" name="same-address">
-          <label class="custom-control-label" for="same-address">Shipping address is the same as my billing address</label>
-        </div>
-        <div class="custom-control custom-checkbox">
-          <input type="checkbox" class="custom-control-input" id="save-info" name="save-info">
-          <label class="custom-control-label" for="save-info">Save this information for next time</label>
-        </div>
-        <hr class="mb-4">
+          @forelse ($locations as $location)
+          <option value="{{$location->id}}">{{$location->regions_name}} область {{$location->name}} район </option>
+          @empty
+          <p>Отсутствуют</p>
+          @endforelse
 
-        <h4 class="mb-3">Payment</h4>
+        </select>
+        @error('districts')
+        <span class="text-danger"> {{$message}} </span>
+        @enderror
+      </div>
 
-        <div class="d-block my-3">
-          <div class="custom-control custom-radio">
-            <input id="credit" name="paymentMethod" type="radio" class="custom-control-input" checked="" required="">
-            <label class="custom-control-label" for="credit">Credit card</label>
-          </div>
-          <div class="custom-control custom-radio">
-            <input id="debit" name="paymentMethod" type="radio" class="custom-control-input" required="">
-            <label class="custom-control-label" for="debit">Debit card</label>
-          </div>
-          <div class="custom-control custom-radio">
-            <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required="">
-            <label class="custom-control-label" for="paypal">PayPal</label>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6 mb-3">
-            <label for="cc-name">Name on card</label>
-            <input type="text" class="form-control" id="cc-name" placeholder="" required="">
-            <small class="text-muted">Full name as displayed on card</small>
-            <div class="invalid-feedback">
-              Name on card is required
-            </div>
-          </div>
-          <div class="col-md-6 mb-3">
-            <label for="cc-number">Credit card number</label>
-            <input type="text" class="form-control" id="cc-number" placeholder="" required="" name="cc-number">
-            <div class="invalid-feedback">
-              Credit card number is required
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-3 mb-3">
-            <label for="cc-expiration">Expiration</label>
-            <input type="text" class="form-control" id="cc-expiration" placeholder="" required="" name="cc-expiration">
-            <div class="invalid-feedback">
-              Expiration date required
-            </div>
-          </div>
-          <div class="col-md-3 mb-3">
-            <label for="cc-cvv">CVV</label>
-            <input type="text" class="form-control" id="cc-cvv" placeholder="" required="" name="cc-cvv">
-            <div class="invalid-feedback">
-              Security code required
-            </div>
-          </div>
-        </div>
-        <hr class="mb-4">
-        <button class="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
-      </form>
+      <div class="col-md-6 mb-3">
+        <label for="pickup">Хранение зерна (текущее состояние сбора):</label>
+        <select class="custom-select d-block w-100" id="pickup" name='pickup' required="">
+          <option value="">Choose...</option>
 
-      @endsection
+          @forelse ($pickup as $type)
+          <option value="{{$type->id}}">{{$type->name}}</option>
+          @empty
+          <p>Отсутствуют</p>
+          @endforelse
+
+        </select>
+      </div>
+
+      <div class="col-md-12 mb-3">
+        <label for="cropdescription">Описание: </label>
+        <textarea class="form-control" id="cropdescription" rows="8" name='cropdescription'></textarea>
+      </div>
+    </div>
+
+    <h4 class="mb-3">Немного о себе</h4>
+
+    <div class="row">
+      <div class="col-md-12 mb-3">
+        <label for="seller_types">Ваш статус, как продавца</label>
+        <select class="custom-select d-block w-100" id="seller_types" name='seller_types' required="">
+          <option value="">Choose...</option>
+
+          @forelse ($seller_types as $seller_type)
+          <option value="{{$seller_type->id}}">{{$seller_type->name}}</option>
+          @empty
+          <p>Отсутствуют</p>
+          @endforelse
+
+        </select>
+      </div>
+
+      <div class="col-md-6 mb-3">
+        <label for="company_web">Company's Website URL</label>
+        <input type="text" class="form-control" id="company_web" name='c_web' placeholder="" required="">
+      </div>
+
+      <div class="col-md-6 mb-3">
+        <label for="phone">Контактный телефон:</label>
+        <input type="text" class="form-control" id="phone" name='phone' placeholder="" required="">
+      </div>
+
+      <div class="col-md-6 mb-3">
+        <label for="payment_methods">Допустимый метод оплаты</label>
+        <select class="custom-select d-block w-100" id="payment_methods" name='payment_methods' required="">
+          <option value="">Choose...</option>
+
+          @forelse ($payment_methods as $payment_method)
+          <option value="{{$payment_method->id}}">{{$payment_method->name}}</option>
+          @empty
+          <p>Отсутствуют</p>
+          @endforelse
+
+        </select>
+      </div>
+
+      <div class="col-md-6 mb-3">
+        <label for="payment_forms">Допустимая форма оплаты</label>
+        <select class="custom-select d-block w-100" id="payment_forms" name='payment_forms' required="">
+          <option value="">Choose...</option>
+
+          @forelse ($payment_forms as $payment_form)
+          <option value="{{$payment_form->id}}">{{$payment_form->name}}</option>
+          @empty
+          <p>Отсутствуют</p>
+          @endforelse
+
+        </select>
+      </div>
+    </div>
+    <button class="btn btn-primary btn-lg btn-block col-md-3" type="submit">Continue</button>
+  </form>
+</div>
+@endsection
